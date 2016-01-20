54ae92aab88d3465a140dbc518fcdaad84ae6af2
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-20 10:24:49 -0800

    example of using ng-click

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ var application = angular.module("myapp", []);
 application.controller("HelloController", function($scope) {
     $scope.hello = {};
     $scope.hello.title = "World";
+
+    $scope.printTitle = function(){
+    	console.log($scope.hello.title);
+    }
 });
